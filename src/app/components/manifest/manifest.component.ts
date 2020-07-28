7a6a8646db90932c876ca568179f351aa0d81005
Yuliana Rojas Serrano <yulirose21@estudiantec.cr>
2020-07-27 19:37:25 -0600

    cualquierestupidez

--- src/app/components/manifest/manifest.component.ts
+++ src/app/components/manifest/manifest.component.ts
@@ -2,12 +2,29 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatAutocomplete, MatAutocompleteSelectedEvent } from '@angular/material';
 import { COMMA, ENTER } from '@angular/cdk/keycodes';
 
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+
 @Component({
   selector: 'app-manifest',
   templateUrl: './manifest.component.html',
   styleUrls: ['./manifest.component.css']
 })
 export class ManifestComponent implements OnInit {
+  displayedColumns: string[] = ['item', 'cost'];
+  
+  transactions = [
+    {item: 'Persona 1', cost: "Adulto"},
+    {item: 'Persona 2', cost: "Adulto"},
+    {item: 'Persona 3', cost: "Adulto"}
+  ];
+
+  step = -1;
 
   start_Date = undefined;
   end_Date = undefined; 
@@ -24,10 +41,37 @@ export class ManifestComponent implements OnInit {
 
   productos = ["Producto A", "Producto B", "Producto C"]
   tipoProductos = ["Tipo A", "Tipo B", "Tipo C"]
-
+ 
   constructor() { }
 
   ngOnInit() {
+    document.getElementById('booking').style.display = 'none';
+    document.getElementById('back').style.display = 'none';
+  }
+
+  back(){
+    document.getElementById('booking').style.display = 'none';
+    document.getElementById('filters').style.display = 'block';
+    document.getElementById('back').style.display = 'none';
+
+  }
+
+  filter() {
+    document.getElementById('booking').style.display = 'block';
+    document.getElementById('filters').style.display = 'none';
+    document.getElementById('back').style.display = 'block';
+  }
+
+  setStep(index: number) {
+    this.step = index;
+  }
+
+  nextStep() {
+    this.step++;
+  }
+
+  prevStep() {
+    this.step--;
   }
 
   dateFilter = (date: Date): boolean => {
